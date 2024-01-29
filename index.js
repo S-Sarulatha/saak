4a38353d0c0a8a49c7437f07700cd5eaa0d2d5e1
sarulatha300@gmail.com <sarulatha300@gmail.com>
2024-01-29 17:16:45 +0530

    project

---
+++ index.js
@@ -0,0 +1,13 @@
+// var sidenav = document.querySelector(".side-nav-bar")
+
+
+
+// function shownavbar() {
+//     sidenav.style.left = "0"
+
+
+// }
+// function hidenavbar() {
+//     sidenav.style.left = "-60px"
+
+// }
