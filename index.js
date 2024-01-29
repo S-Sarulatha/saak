ec140ba775e2ebd65a21344246c11242494a450a
Akshaya2609 <akshayarajkumar2609@gmail.com>
2024-01-29 22:52:17 +0530

    js file errors detected and corrected

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// var sidenav = document.querySelector(".side-nav-bar")
+var sidenav=document.querySelector(".side-nav-bar")
 
 
+function showNavbar(){
+    sidenav.style.left="0"
+}
+function hideNavbar(){
+    sidenav.style.left="-60%"
 
-// function shownavbar() {
-//     sidenav.style.left = "0"
-
-
-// }
-// function hidenavbar() {
-//     sidenav.style.left = "-60px"
-
-// }
+}
